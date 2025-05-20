e6e26cecac2b9f09e9e2dcb1d72ec5ff2740a20b
Yogurtshi <yoshi.gyger@bbzbl-it.ch>
2025-05-20 16:22:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,46 @@
-Hello Yume I like you lots <3
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Adressbuch</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/stylesheet.css">
+  <link rel="icon" type="" href="img/favicon.ico" />
+</head>
+
+<body>
+
+  <div class="container">
+
+    <header></a></header>
+
+    <main>
+      <div class="row websites">
+        <h1>WILKOMMEN ZU MEINER WEBSEITE</h1>
+        <h3>Hier kannst du auf all meine Webseiten zugreifen.</h3>
+        <a href="../portfolio">Portfolio</a>
+        <a href="../old">Old</a>
+        <a href="../tribut">Tribut</a>
+        <a href="../adressbuch/">Adressbuch</a>
+        <a href="../umfrage">Umfrage</a>
+        <a href="../valentinesday">Valentines day</a>
+      </div> 
+    </main>
+
+  </div>
+
+<footer>
+</footer>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+</script>
+
+</body>
+
+</html>
